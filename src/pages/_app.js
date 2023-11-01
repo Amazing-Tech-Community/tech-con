866810812311bb96d1ca4b34bad49986596c23a4
Yhucee14 <uchechukwu7814@gmail.com>
2023-11-01 10:57:55 -0700

    added some scripts

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,15 +1,10 @@
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
-import '../styles/globals.css'
-// import { Inter } from 'next/font/google'
-import Head from 'next/head'
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import '../styles/globals.css';
+import Head from 'next/head';
+import Script from 'next/script'; // Import the Script component
 
-// const inter = Inter({
-//   subsets: ['latin'],
-//   variable: '--font-inter'
-// })
-
-export default function App({ Component, pageProps }) {
+function App({ Component, pageProps }) {
   return (
     <>
       <Head>
@@ -17,18 +12,35 @@ export default function App({ Component, pageProps }) {
         <link rel="icon" href="/favicon.png" />
         <link
           rel="stylesheet"
-          href="/fonts/SF-Text-Medium.otf" 
+          href="/fonts/SF-Text-Medium.otf"
           as="font"
           type="font/otf"
           crossOrigin="anonymous"
         />
       </Head>
-
+      <Script
+        strategy="afterInteractive" // Choose an appropriate loading strategy
+        src="https://assets.mailerlite.com/js/universal.js"
+        id="mailerlite-script"
+        async
+      >
+        {`
+          (function(w,d,e,u,f,l,n){
+            w[f]=w[f]||function(){
+              (w[f].q=w[f].q||[]).push(arguments);
+            },l=d.createElement(e),l.async=1,l.src=u,
+            n=d.getElementsByTagName(e)[0],n.parentNode.insertBefore(l,n);
+          })(window,document,'script','https://assets.mailerlite.com/js/universal.js','ml');
+          ml('account', '670955');
+        `}
+      </Script>
       <main className={`font-sf-pro bg-[#1e1e1e] text-[#fafafa]  px-3 `}>
         <Navbar />
         <Component {...pageProps} />
         <Footer />
       </main>
     </>
-  )
+  );
 }
+
+export default App;
